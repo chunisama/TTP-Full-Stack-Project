29d77c3fe01c1936e3157cfcf235566d45e30508
“Eugene <eugenechung1209@gmail.com>
2020-02-17 12:51:16 -0500

    finish routes

--- app.js
+++ app.js
@@ -1,15 +1,16 @@
-const mongoose = require('mongoose');
 const express = require("express");
 const app = express();
-const db = require('./config/keys').mongoURI;
+const mongoose = require('mongoose');
+const db = require('./config/keys.js').mongoURI;
+const users = require("./routes/api/users");
 
 mongoose
-  .connect(db, { useNewUrlParser: true })
+  .connect(db, { useNewUrlParser: true } )
   .then(() => console.log("Connected to MongoDB successfully"))
   .catch(err => console.log(err));
 
 app.get("/", (req, res) => res.send("Hello World"));
-
+app.use("/api/users", users);
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server is running on port ${port}`));
