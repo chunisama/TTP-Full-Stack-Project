8debadbe853107f1cd0d1ff6671f53e1a9db0c7f
“Eugene <eugenechung1209@gmail.com>
2020-02-17 13:11:53 -0500

    fixed app title

--- app.js
+++ app.js
@@ -3,14 +3,23 @@ const app = express();
 const mongoose = require('mongoose');
 const db = require('./config/keys.js').mongoURI;
 const users = require("./routes/api/users");
+const bodyParser = require('body-parser');
 
 mongoose
   .connect(db, { useNewUrlParser: true } )
   .then(() => console.log("Connected to MongoDB successfully"))
   .catch(err => console.log(err));
 
-app.get("/", (req, res) => res.send("Hello World"));
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+
 app.use("/api/users", users);
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 const port = process.env.PORT || 5000;
-app.listen(port, () => console.log(`Server is running on port ${port}`));
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
