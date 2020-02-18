b3804a5afb4357ee4c6a956b5bcd05ff0c8ad872
“Eugene <eugenechung1209@gmail.com>
2020-02-18 02:47:14 -0500

    finished redux slice for user session data

--- frontend/src/reducers/root_reducer.js
+++ frontend/src/reducers/root_reducer.js
@@ -1,10 +1,10 @@
-// src/reducers/root_reducer.js
-
 import { combineReducers } from 'redux';
 import session from './session_reducer';
+import error from './errors_reducer';
 
 const RootReducer = combineReducers({
   session,
+  error,
 });
 
 export default RootReducer;
