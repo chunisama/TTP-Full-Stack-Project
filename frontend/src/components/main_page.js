d0578ea562bb251113701154b9bb24a64e195cb2
“Eugene <eugenechung1209@gmail.com>
2020-02-18 02:35:54 -0500

    finished skeleton for redux cycle in my react front end

---
+++ frontend/src/components/main_page.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class MainPage extends React.Component {
+
+  render() {
+    return (
+      <div>
+        <h1>TTP NYC Full Stack</h1>
+        <footer>
+          Copyright &copy; 2020 Chung 
+        </footer>
+      </div>
+    );
+  }
+}
+
+export default MainPage;
