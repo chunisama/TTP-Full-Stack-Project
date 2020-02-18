4490e4e933830378d3c3ab495f0bcff1e359993d
“Eugene <eugenechung1209@gmail.com>
2020-02-17 20:54:10 -0500

    test new protected routes, registration routes, login routes, etc

--- app.js
+++ app.js
@@ -2,17 +2,24 @@ const express = require("express");
 const app = express();
 const mongoose = require('mongoose');
 const db = require('./config/keys.js').mongoURI;
-const users = require("./routes/api/users");
 const bodyParser = require('body-parser');
+const passport = require('passport');
+
+const users = require("./routes/api/users");
 
 mongoose
-  .connect(db, { useNewUrlParser: true } )
+  .connect(db, { useNewUrlParser: true,  useUnifiedTopology: true })
   .then(() => console.log("Connected to MongoDB successfully"))
   .catch(err => console.log(err));
 
 app.get("/", (req, res) => {
+  // console.log(res);
+  // debugger;
   res.send("Hello World");
 });
+
+app.use(passport.initialize());
+require('./config/passport')(passport);
 
 app.use("/api/users", users);
 app.use(bodyParser.urlencoded({ extended: false }));
