b30fb460de3d4d24c869311e4d5fe4d50e3ced8c
“Eugene <eugenechung1209@gmail.com>
2020-02-24 20:50:07 -0500

    fix deployment bugs

---
+++ frontend/build/precache-manifest.4b45df2d163328e6e4c6236177ac7f7a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7bb1f1d3ae9d35c393babb98972474fc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b0f22138548672926f14",
+    "url": "/static/css/main.9e1e3638.chunk.css"
+  },
+  {
+    "revision": "14932ce8d30060b5d55e",
+    "url": "/static/js/2.c42bb948.chunk.js"
+  },
+  {
+    "revision": "d891fcc30dfc707e593ab65e09f6fef9",
+    "url": "/static/js/2.c42bb948.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b0f22138548672926f14",
+    "url": "/static/js/main.8227a64a.chunk.js"
+  },
+  {
+    "revision": "a8139511e7e3c6f8467f",
+    "url": "/static/js/runtime-main.1c2d59f4.js"
+  }
+]);
